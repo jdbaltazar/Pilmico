34fcf6b7c6a1e014d370f23f8ef8542659a46ce4
diablo122 <nlptrkybnz@yahoo.com>
2013-07-19 16:06:09 +0800

    showed loginpanel

--- src/gui/CenterPanel.java
+++ src/gui/CenterPanel.java
@@ -77,21 +77,21 @@ public class CenterPanel extends SoyPanel {
 
 	private void addComponents() {
 
-		// add(new LoginPanel());
+		 add(new LoginPanel());
 
-		try {
-			Manager.getInstance().login("manager", "pilmico".toCharArray());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		changeTable(Values.HOME);
+//		try {
+//			Manager.getInstance().login("manager", "pilmico".toCharArray());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		changeTable(Values.HOME);
 
 	}
 
 	public void changeTable(int val) {
 
-		if (val != Values.HOME)
+//		if (val != Values.HOME)
 			remove(getComponent(getComponentCount() - 1));
 		// remove(getComponent(0));
 
