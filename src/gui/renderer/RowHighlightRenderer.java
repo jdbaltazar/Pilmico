f31970a252cfb0df3081c0035b3ce8c1f8fe3a59
johndavidbaltazar <johndavidbaltazar@yahoo.com.ph>
2013-08-17 20:07:01 +0800

    undo edit in rowhighlightrenderer

--- src/gui/renderer/RowHighlightRenderer.java
+++ src/gui/renderer/RowHighlightRenderer.java
@@ -33,13 +33,14 @@ public class RowHighlightRenderer extends DefaultTableCellRenderer {
 	private static final long serialVersionUID = 1L;
 	Set<Integer> highlightedRows = new HashSet<Integer>();
 	Color selectedColor = new Color(0, 255, 255), backgroundColor1 = new Color(245, 245, 220), backgroundColor2 = new Color(220, 220, 220),
-			invalidColor = Color.decode("#F9CCCC");//= new Color(255, 140, 140);
+			invalidColor = Color.decode("#F9CCCC");// = new Color(255, 140, 140);
 
 	private boolean valid = true;
-//	private int alertOnQuantity, quantity, itemId;
-	
-	public RowHighlightRenderer(){
-		
+
+	// private int alertOnQuantity, quantity, itemId;
+
+	public RowHighlightRenderer() {
+
 	}
 
 	@SuppressWarnings("unchecked")
@@ -47,7 +48,7 @@ public class RowHighlightRenderer extends DefaultTableCellRenderer {
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
-//		table.setToolTipText(value.toString());
+		table.setToolTipText(value.toString());
 
 		setHorizontalAlignment(SwingConstants.CENTER);
 
@@ -61,83 +62,82 @@ public class RowHighlightRenderer extends DefaultTableCellRenderer {
 			setForeground(Color.BLACK);
 		}
 
-		if(table.getName().equals(Tables.SALES)){
+		if (table.getName().equals(Tables.SALES)) {
 			List<Sales> sales = (List<Sales>) Values.tablePanel.getSoyTable().getObjects();
 			Sales object = sales.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
-			
-		}
-		else if(table.getName().equals(Tables.EXPENSES)){
+
+		} else if (table.getName().equals(Tables.EXPENSES)) {
 			List<DailyExpenses> expense = (List<DailyExpenses>) Values.tablePanel.getSoyTable().getObjects();
 			DailyExpenses object = expense.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.SALARY)){
+
+		else if (table.getName().equals(Tables.SALARY)) {
 			List<SalaryRelease> sr = (List<SalaryRelease>) Values.tablePanel.getSoyTable().getObjects();
 			SalaryRelease object = sr.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.DELIVERIES)){
+
+		else if (table.getName().equals(Tables.DELIVERIES)) {
 			List<Delivery> delivery = (List<Delivery>) Values.tablePanel.getSoyTable().getObjects();
 			Delivery object = delivery.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.PULLOUTS)){
+
+		else if (table.getName().equals(Tables.PULLOUTS)) {
 			List<PullOut> po = (List<PullOut>) Values.tablePanel.getSoyTable().getObjects();
 			PullOut object = po.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.ACCOUNT_RECEIVABLES)){
+
+		else if (table.getName().equals(Tables.ACCOUNT_RECEIVABLES)) {
 			List<AccountReceivable> ar = (List<AccountReceivable>) Values.tablePanel.getSoyTable().getObjects();
 			AccountReceivable object = ar.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.AR_PAYMENTS)){
+
+		else if (table.getName().equals(Tables.AR_PAYMENTS)) {
 			List<ARPayment> arp = (List<ARPayment>) Values.tablePanel.getSoyTable().getObjects();
 			ARPayment object = arp.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.CASH_ADVANCE)){
+
+		else if (table.getName().equals(Tables.CASH_ADVANCE)) {
 			List<CashAdvance> ca = (List<CashAdvance>) Values.tablePanel.getSoyTable().getObjects();
 			CashAdvance object = ca.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.CA_PAYMENTS)){
+
+		else if (table.getName().equals(Tables.CA_PAYMENTS)) {
 			List<CAPayment> cap = (List<CAPayment>) Values.tablePanel.getSoyTable().getObjects();
 			CAPayment object = cap.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.DISCOUNTS)){
+
+		else if (table.getName().equals(Tables.DISCOUNTS)) {
 			List<DiscountIssue> di = (List<DiscountIssue>) Values.tablePanel.getSoyTable().getObjects();
 			DiscountIssue object = di.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
-		else if(table.getName().equals(Tables.DEPOSITS)){
+
+		else if (table.getName().equals(Tables.DEPOSITS)) {
 			List<Deposit> d = (List<Deposit>) Values.tablePanel.getSoyTable().getObjects();
 			Deposit object = d.get(table.convertRowIndexToModel(row));
-			
+
 			valid = object.isValid();
 		}
-		
+
 		if (!valid) {
 			setBackground(invalidColor);
 		}
